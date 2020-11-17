df8d5cf77ceeaada2561bb6291db10db4e47d647
Guillaume Keerhem <keerhem.guillaume@hotmail.fr>
2020-11-17 16:58:00 +0100

    navigation

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,15 +1,15 @@
 function openPage(pageName, elmnt, color) {
   // Hide all elements with class="tabcontent" by default */
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+  var i, contenu, menu;
+  contenu = document.getElementsByClassName("contenu");
+  for (i = 0; i < contenu.length; i++) {
+    contenu[i].style.display = "none";
   }
 
   // Remove the background color of all tablinks/buttons
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
+  menu = document.getElementsByClassName("menu");
+  for (i = 0; i < menu.length; i++) {
+    menu[i].style.backgroundColor = "";
   }
 
   // Show the specific tab content
