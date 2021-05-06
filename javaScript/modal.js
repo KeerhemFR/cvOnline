acd0e42ab21df114e3dd0ec125350f754c7306c3
guillaume keerhem <keerhem.guillaume@hotmail.fr>
2021-05-06 10:45:37 +0200

    carousel portfolio

--- javaScript/modal.js
+++ javaScript/modal.js
@@ -1,6 +1,27 @@
 // modal related in JQuery
 $(document).ready(function(){
-  // dice roll modal on / off
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+              nav:true,
+              loop:true,
+          },
+          600:{
+              items:1,
+              nav:true,
+              loop:true,
+          },
+          1000:{
+              items:1,
+              nav:true,
+              loop:true,
+          }
+      }
+  })  // dice roll modal on / off
   $("#modal-1-open").on("click", function(){
     $("#modal-1").css("visibility","visible");
   });
